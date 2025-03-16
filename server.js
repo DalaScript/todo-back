cf1c4a2005a0e4d5669f3c5ac6baf5a2577732f4
Dalala <nik.dalalishvili@gmail.com>
2025-03-16 23:03:50 +0100

    implement start of server

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "content-type": "text/html"
+    });
+    res.end(JSON.stringify({ message: "Server is running on port: " + PORT }));
+});
+
+server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+
+});
