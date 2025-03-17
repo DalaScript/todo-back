f42a4ed8dfd595986246308b3b115cd1f9cf7c04
Dalala <nik.dalalishvili@gmail.com>
2025-03-17 09:59:28 +0100

    implement tasks in mongodb databas

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,23 +1,32 @@
-export function handleTasksRoutes(req, res) {
+import { getDB } from "../db/db.js";
+
+export async function handleTasksRoutes(req, res) {
+    const db = await getDB();
+    const taskCollection = db.collection("tasks");
+
     if(req.method === "GET" && req.url === "/tasks"){
+        const tasks = await taskCollection.find({}).toArray();
         res.writeHead(200, {
             "Content-Type": "application/json",
             "cache-control": "no-cache"
         })
-        res.end(JSON.stringify({ message: "Tasks fetched successfully!" }));
+        res.end(JSON.stringify(tasks));
     }else if(req.method === "POST" && req.url === "/tasks") {
         let body = "";
         req.on("data", (chunk) => {
             body += chunk;
         });
-        req.on("end", () => {
+        req.on("end", async () => {
             const newTask = JSON.parse(body);
+            const result = await taskCollection.insertOne(newTask);
             res.writeHead(201, {
                 "Content-Type": "application/json",
                 "cache-control": "no-cache"
             });
             res.end(
-                JSON.stringify({ message: "Task created successfully!", tasks: newTask })
+                JSON.stringify({ 
+                    message: "Task created successfully!", 
+                    taskId: result.insertedId })
             );
         });
     }else {
